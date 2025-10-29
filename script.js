5cac87d4757698571a9313a8d640ff659d6fc961
MiguelAVL09 <mikelun3@gmail.com>
2025-10-28 20:14:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -122,7 +122,7 @@ document.addEventListener('DOMContentLoaded', () => {
             
             try {
                 // Llama a nuestro servidor en el puerto 3000
-                const response = await fetch('https://lenguas-indigenas-ia.onrender.com/chat', {
+                const response = await fetch('https://lenguas-indigenas-ia.onrender.com', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
